d5e2cdbc8f310b9abc00120f3543b4ecf2bf13c4
ShenHaodong <2282935827@qq.com>
2019-07-14 21:10:16 +0800

    init commit

---
+++ src/main/java/com/shenhd/learn/simplespringbootproject/SimpleSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shenhd.learn.simplespringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringbootProjectApplication.class, args);
+    }
+
+}
